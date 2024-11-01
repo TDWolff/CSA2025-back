20549d61b3a4a0cacf6211c8c6d5dd90bc4bcb67
TDWolff <torin.dean.wolff@gmail.com>
2024-11-01 13:58:30 -0700

    updating landing page

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from init import app, port, selftitle
-
 from api.user import *
 
 app = Flask(__name__)
@@ -11,6 +10,11 @@ app.register_blueprint(user_api)
 def home():
     title = selftitle
     return render_template('index.html', title=title)
+
+@app.route('/landing')
+def landing():
+    title = selftitle
+    return render_template('landing.html', title=title)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)
